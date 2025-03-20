281b0d5a256ef96bee11aceafc3acba459a01067
Ella Hou <yipingella369@gmail.com>
2025-03-20 18:20:23 -0400

    fixed bug in increase_testing_total

--- Program.h
+++ Program.h
@@ -17,20 +17,20 @@ sample_frequencies hash map and a testing vector to guide Batch creation.
 #include <unordered_map>
 
 using namespace std;
-const int SOURCE_SPOTS = 96;
+//number of spots in a rack, usually 96
+const int RACK_CAPACITY = 96;
 
 class Program {
 public:
 	//constructor
-	Program(int rack_capacity) {
-		this->rack_capacity = rack_capacity;
+	Program() {
 
 		//initialize sample_frequencies array
-		for (int i = 0; i < SOURCE_SPOTS + 1; i++) {
+		for (int i = 0; i < RACK_CAPACITY + 1; i++) {
 			sample_frequencies.push_back(0);
 		}
 	}
-	
+
 	//read and analyze data about the rack sample numbers
 	void read_data();
 	void populate_frequencies();
@@ -43,9 +43,6 @@ public:
 	void export_results();
 
 private:
-	//number of spots in a rack, usually 96
-	int rack_capacity;  
-	
 	//temporary vector finding the best combination of sample numbers before modifying actual all_sources array, cleared with each new batch
 	vector<int> testing_array;
 
@@ -73,8 +70,8 @@ private:
 
 
 	//higher-level methods for creating batches
-	void create_new_batch(int num_source_spots);
-	int choose_num_sources();
+	void create_new_batch(int num_source_spots, vector<int> example_testing_array);
+	int choose_num_sources(vector<int>& example_testing_array);
 	void add_all_except_last(int num_source_spots);
 	void add_ratios(int num_source_spots);
 	Batch finalize_spots();
@@ -87,8 +84,8 @@ private:
 	int find_smallest();
 	int ideal_last(int num_source_spots);
 	int total_testing_samples();
-	void decrease_testing_total(int &ideal_last_spot, int &to_add, int &num_source_racks);
-	void increase_testing_total(int& ideal_last_spot, int& to_add, int& num_source_racks);
+	void decrease_testing_total(int& ideal_last_spot, int& to_add, int& num_source_racks);
+	void increase_testing_total(int& ideal_last_spot, int& to_add, int& num_source_racks, vector<int> example_testing_array, bool &approximate);
 	void add_in_order(vector<int>& which_vector, int to_add);
 	void remove_from_testing(int to_remove);
 	int find_next_smallest_valid(int current);
